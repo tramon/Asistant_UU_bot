0428cade119494c45f0affedd6957dcdd8a2ff48
tramon <andriitramon@gmail.com>
2026-06-25 14:18:02 +0300

    alias docs for doc command

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ async def main():
     app.add_handler(CommandHandler("info", info))
     app.add_handler(CommandHandler("week", week))
     app.add_handler(CommandHandler("schedule", schedule))
-    app.add_handler(CommandHandler("doc", doc))
+    app.add_handler(CommandHandler(["doc", "docs"], doc))
     app.add_handler(CommandHandler(["reload", "reset", "restart"], reload_scheduler))
     app.add_handler(CallbackQueryHandler(week_callback, pattern="^week$"))
     app.add_handler(CallbackQueryHandler(schedule_callback, pattern="^schedule$"))
